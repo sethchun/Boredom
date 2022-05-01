f67c6cdc653ea8b841b7de37a2dc126ee29e0aa6
Seth Chun <schun.code@gmail.com>
2022-05-01 10:06:53 -0500

    Add computer choice and documentation

--- rock-paper-scissors/main.js
+++ rock-paper-scissors/main.js
@@ -1,5 +1,8 @@
+// Collect user input
 let userInput = prompt('Let\'s play Rock, Paper Scissors! What do you choose?');
 
+
+// Display user input
 if (userInput.toLowerCase() == 'rock') {
     console.log(`User chooses: ${userInput}`);
 };
@@ -8,4 +11,18 @@ if (userInput.toLowerCase() == 'paper') {
 };
 if (userInput.toLowerCase() == 'scissors') {
     console.log(`User chooses: ${userInput}`);
+};
+
+// Computer choice
+let randomNum = Math.floor(Math.random() * 3); // Random Num from 0 to 2
+
+// Display computer choice
+if (randomNum === 0) {
+    console.log('Computer chooses: rock');
+};
+if (randomNum === 1) {
+    console.log('Computer chooses: paper');
+};
+if (randomNum === 2) {
+    console.log('Computer chooses: scissors');
 };
