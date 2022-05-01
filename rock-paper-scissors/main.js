703ce98eafb30a4ba80c5b5fe94fb1f3d8b0baec
Seth Chun <schun.code@gmail.com>
2022-04-30 19:07:09 -0500

    Added user input

---
+++ rock-paper-scissors/main.js
@@ -0,0 +1,11 @@
+let userInput = prompt('Let\'s play Rock, Paper Scissors! What do you choose?');
+
+if (userInput.toLowerCase() == 'rock') {
+    console.log(`User chooses: ${userInput}`);
+};
+if (userInput.toLowerCase() == 'paper') {
+    console.log(`User chooses: ${userInput}`);
+};
+if (userInput.toLowerCase() == 'scissors') {
+    console.log(`User chooses: ${userInput}`);
+};
